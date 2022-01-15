7ef13d37b57302fa375276511a9d52513ed48b1d
Victor-El <victorelezua@gmail.com>
2022-01-15 05:56:15 +0100

    update port

--- src/main/java/me/codeenzyme/App.java
+++ src/main/java/me/codeenzyme/App.java
@@ -5,7 +5,20 @@ import com.blade.Blade;
 public class App {
 
     public static void main(String[] args) {
-        Blade.of().get("/", ctx -> {
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 9000;
+        }
+
+        Blade.of().listen(port).get("/", ctx -> {
             ctx.text("Welcome to root");
         }).start();
     }
